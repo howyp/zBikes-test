812220cc328e01a854d81e4b061dcb06be521623
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-09-02 09:44:58 +0100

    Test that bike IDs are re-assigned when a station is updated

--- src/test/scala/RESTSpec.scala
+++ src/test/scala/RESTSpec.scala
@@ -1,6 +1,6 @@
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 import scalaj.http._
 
@@ -11,7 +11,7 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures {
     |SO THAT bikes can be hired
   """.stripMargin - {
     "PUT /station/<id>" - {
-      "Create/update a station with a set of bike IDs" in {
+      "Create a station with a set of bike IDs" in {
         val resp = PUT("/station/12345") {"""{
                                                "name": "West Road",
                                                "location": {
@@ -25,8 +25,7 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures {
         resp should (have('code(200)) or have ('code (201)))
         resp.headers should contain ("Location" -> "/station/12345")
 
-        val url = resp.headers("Location")
-        GET(url).body should be(Json.parse("""{
+        GET(resp.headers("Location")).body should be(Json.parse("""{
                                      "name": "West Road",
                                      "location": {
                                        "lat": 3.20,
@@ -36,6 +35,63 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures {
                                        "001","002","003","004"
                                      ]
                                    }"""))
+      }
+      "Update an existing station with new details" in {
+        GET("/station/12345").body should be(Json.parse("""{
+                                     "name": "West Road",
+                                     "location": {
+                                       "lat": 3.20,
+                                       "long": 40.24
+                                     },
+                                     "availableBikes": [
+                                       "001","002","003","004"
+                                     ]
+                                   }"""))
+        val resp = PUT("/station/12345") {"""{
+                                               "name": "South Road",
+                                               "location": {
+                                                 "lat": 3.23,
+                                                 "long": 40.21
+                                               },
+                                               "availableBikes": [
+                                                 "001","003","005"
+                                               ]
+                                             }"""}
+        resp should (have('code(200)) or have ('code (201)))
+        GET("/station/12345").body should be(Json.parse("""{
+                                                           "name": "South Road",
+                                                           "location": {
+                                                             "lat": 3.23,
+                                                             "long": 40.21
+                                                           },
+                                                           "availableBikes": [
+                                                             "001","003","005"
+                                                           ]
+                                                         }"""))
+      }
+      "Remove given bike IDs from other stations" in {
+        GET("/station/12345").body should be(Json.parse("""{
+                                                           "name": "South Road",
+                                                           "location": {
+                                                             "lat": 3.23,
+                                                             "long": 40.21
+                                                           },
+                                                           "availableBikes": [
+                                                             "001","003","005"
+                                                           ]
+                                                         }"""))
+        PUT("/station/67890") {"""{
+                                   "name": "East Road",
+                                   "location": {
+                                     "lat": 3.03,
+                                     "long": 40.01
+                                   },
+                                   "availableBikes": [
+                                     "005"
+                                   ]
+                                 }"""} should (have('code(200)) or have ('code (201)))
+        (GET("/station/12345").body \ "availableBikes").get should be (Json.parse("""["001","003"]"""))
+        (GET("/station/67890").body \ "availableBikes").get should be (Json.parse("""["005"]"""))
       }
     }
   }
@@ -54,9 +110,9 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures {
     "List locations with low number of bikes, together with near locations that have many bikes (ie. candidates for balancing)" in {}
   }
 
-  def GET(url: String): HttpResponse[JsValue] =
+  def GET(url: String): HttpResponse[JsObject] =
     Http("http://localhost:9000" + url)
-      .execute(is => Json.parse(HttpConstants.readString(is)))
+      .execute(is => Json.parse(HttpConstants.readString(is)).as[JsObject])
 
   def PUT(url: String)(json: String): HttpResponse[String] =
     Http("http://localhost:9000" + url)
