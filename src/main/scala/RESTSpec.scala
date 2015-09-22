6815bdf88bfe1ec948c6cd24264599ddb86316b8
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-09-22 22:14:55 +0100

    Remove dead code, improve matcher messages

--- src/main/scala/RESTSpec.scala
+++ src/main/scala/RESTSpec.scala
@@ -1,19 +1,15 @@
 
-import java.io.InputStream
 import com.github.tomakehurst.wiremock._
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration._
-import org.scalactic.Fail
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.{MatchResult, Matcher}
+import spray.json._
 
 import scala.util.{Failure, Success, Try}
 import scalaj.http._
-
-import spray.json._
-import DefaultJsonProtocol._
 
 class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAfterAll {
 
@@ -45,7 +41,7 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
                                       "001","002","003","004"
                                     ]
                                   }""") `should respond with` (
-          (`status code` (200) or `status code` (201)) and `Location header`("/station/12345")
+          `Location header`("/station/12345") and (`status code` (200) or `status code` (201))
         )
 
         GET("/station/12345") `should respond with` (`status code`(200) and body("""{
@@ -338,14 +334,6 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
     }
   }
 
-  def GET_old(url: String): HttpResponse[JsObject] = {
-    info(s"GET $url")
-    val request =
-      if (url.startsWith("http")) Http(url)
-      else Http("http://localhost:9000" + url)
-    request.execute(parseBodyAsJson)
-  }
-
   def GET(url: String, attempt: Int = -1) = TestStep(s"GET", attempt)(url)(None)
   def DELETE(url: String, attempt: Int = -1) = TestStep(s"DELETE", attempt)(url)(None)
   def PUT(url: String, attempt: Int = -1)(requestBody: String) = TestStep(s"PUT", attempt)(url)(Some(requestBody))
@@ -371,39 +359,6 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
     }
   }
 
-  def PUT_old(url: String)(json: String): HttpResponse[JsObject] = {
-    info(s"PUT $url ${json.parseJson}")
-    Http("http://localhost:9000" + url)
-      .postData(json)
-      .header("Content-Type", "application/json")
-      .method("PUT")
-      .execute(parseBodyAsJson)
-  }
-
-  def POST_old(url: String)(json: String): HttpResponse[JsObject] = {
-    info(s"POST $url ${json.parseJson}")
-    Http("http://localhost:9000" + url)
-      .postData(json)
-      .header("Content-Type", "application/json")
-      .method("POST")
-      .execute(parseBodyAsJson)
-  }
-
-  def parseBodyAsJson(is: InputStream): JsObject =
-    Try(HttpConstants.readString(is))
-      .map {
-      case "" => JsObject()
-      case other => other.parseJson.asJsObject
-    } match {
-      case Failure(exception) => fail(s"Could not parse JSON from response: ${exception.getMessage}")
-      case Success(json) => json
-    }
-
-
-  def tupleOfUrls(body: JsValue): (String, String) = body.asJsObject.getFields("selfUrl", "hireUrl") match {
-    case Seq(JsString(selfUrl), JsString(hireUrl)) => (selfUrl, hireUrl)
-  }
-
   def `status code`(expected: Int) = Matcher[HttpResponse[_]] { case r@ HttpResponse(_, actual, _) =>
     MatchResult(
       matches = actual == expected,
@@ -414,7 +369,7 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
 
   def `Location header`(expected: String) = Matcher[HttpResponse[_]] { case r@ HttpResponse(_, _, headers) =>
     headers.get("Location") match {
-      case None => fail(s"Response $r did not have a Location header")
+      case None => fail(s"Response headers $headers did not have a Location header")
       case Some(actual) => MatchResult(
         matches = actual endsWith expected,
         rawFailureMessage = s"Response Location header '$actual' did not end with '$expected'",
@@ -426,13 +381,13 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
   def body(expected: String) = Matcher[HttpResponse[String]] { case r@ HttpResponse(actual, _, _) =>
     val expectedJ = expected.parseJson
     actual match {
-      case "" => MatchResult(false, s"Response body was empty, expected ${expectedJ.prettyPrint}", "")
+      case "" => MatchResult(false, s"Response body was empty, expected ${expectedJ}", "")
       case other => Try(other.parseJson.asJsObject) match {
         case Failure(_) => MatchResult(false, s"Response body '${other.take(20)}...' was not JSON", "")
         case Success(actualJ) => MatchResult(
           matches = actualJ == expectedJ,
-          rawFailureMessage = s"Response body $actualJ was not ${expectedJ.prettyPrint}",
-          rawNegatedFailureMessage = s"Response body was not ${expectedJ.prettyPrint}"
+          rawFailureMessage = s"Response body $actualJ was not ${expectedJ}",
+          rawNegatedFailureMessage = s"Response body was not ${expectedJ}"
         )
       }
     }
@@ -441,12 +396,13 @@ class RESTSpec extends FreeSpec with Matchers with ScalaFutures with BeforeAndAf
   def bodyProperty(name: String, expected: String) = Matcher[HttpResponse[String]] { case r@ HttpResponse(actual, _, _) =>
     val expectedJ = expected.parseJson
     actual match {
-      case "" => MatchResult(false, s"Response body was empty, not ${expectedJ.prettyPrint} at property $name", "")
-      case other => Try(other.parseJson.asJsObject) match {
-        case Failure(_) => MatchResult(false, s"Response body '${other.take(20)}...' was not JSON, expected ${expectedJ.prettyPrint} at property $name", "")
-        case Success(actualJ) => MatchResult(
-          matches = actualJ.fields(name) == expectedJ,
-          rawFailureMessage = s"Response body $actualJ was not ${expectedJ.prettyPrint} at property $name",
+      case "" => MatchResult(false, s"Response body was empty, not ${expectedJ} at property $name", "")
+      case other => Try(other.parseJson.asJsObject).map(j => (j.fields.get(name), j)) match {
+        case Failure(_) => MatchResult(false, s"Response body '${other.take(20)}...' was not JSON, expected ${expectedJ} at property $name", "")
+        case Success((None, wholeBody)) => MatchResult(false, s"Response body $wholeBody was not JSON, expected ${expectedJ} at property $name", "")
+        case Success((Some(fieldValue), wholeBody)) => MatchResult(
+          matches = fieldValue == expectedJ,
+          rawFailureMessage = s"Response body $wholeBody was not ${expectedJ} at property $name",
           rawNegatedFailureMessage = s"negated matching not supported"
         )
       }
